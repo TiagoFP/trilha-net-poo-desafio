fe4e1ab9245774decb7ca936fab00e0228ed2843
TiagoFP <tiagopimenteldev@gmail.com>
2023-10-22 16:53:38 -0300

    Complementando o código

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Iphone:");
+Smartphone iphone = new Iphone(numero: "55446699", modelo: "13", imei: "1111111", memoria: 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("-------------------------------");
+
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia(numero: "22665588", modelo: "6120", imei: "2222222", memoria: 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
